83df40ed79e4c32c3ed19ada352f6cb995fb13d8
waltiu <waltiu@163.com>
2023-04-21 19:04:18 +0800

    feat:  get 请求刷新数据  post 走阿里云自动更新

--- watch-image/index.js
+++ watch-image/index.js
@@ -1,12 +1,12 @@
 /**
  * 请求url参数
- * port 服务端口号 必填
- * containerName  容器名称 必填
- * nameSpace  阿里云容器命名空间 选填
- * name  阿里云实例名称 选填
- * version  版本 选填
+ * nameSpace  阿里云容器命名空间
+ * name  阿里云实例名称
+ * version  版本
+ * port 服务端口号
+ * containerName  容器名称
  */
-const { exec, execSync  } = require("child_process");
+const { exec, execSync } = require("child_process");
 const http = require("http");
 const url = require("url");
 
@@ -28,6 +28,7 @@ const resolvePost = (req) => {
 http
   .createServer(async (req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
+    let shellUrl = "";
     const { pathname, query } = url.parse(req.url, true);
     if (req.method === Type_GET) {
       const response = {
@@ -38,6 +39,10 @@ http
         query,
       };
       console.log(new Date().toLocaleString(), response);
+      const path = `${query.nameSpace}/${query.name}:${query.version}`;
+      shellUrl = `update.sh ${path}  ${query.port}  ${query.containerName}`;
+      console.log(shellUrl, "shell");
+      execSync(shellUrl);
       res.end(JSON.stringify(response));
     }
     if (req.method === TYPE_POST) {
@@ -50,18 +55,13 @@ http
         containerName: query.containerName,
       };
       console.log(new Date().toLocaleString(), response);
-
-      const path = `${query.nameSpace || result.repository.nameSpace}/${
-        query.name || result.repository.name
-      }:${query.version || result.push_data.tag}`;
-
-      const shell=` update.sh ${path}  ${response.port}  ${query.containerName}`
-      console.log(shell,'shell')
-      execSync(shell)
-
-
+      const path = `${result.repository.nameSpace}/${result.repository.name}:${result.push_data.tag}`;
+      shellUrl = ` update.sh ${path}  ${response.port}  ${query.containerName}`;
+      console.log(shellUrl, "shell");
+      execSync(shellUrl);
       res.end(JSON.stringify(response));
     }
+
   })
   .listen(3000, () => {
     console.log("server is ready");
--- watch-image/update.sh
+++ watch-image/update.sh
@@ -7,7 +7,7 @@ password=$5
 imagepath=registry.cn-beijing.aliyuncs.com/$path
 
 
-# 这里大家可以提前在安装依赖的时候就执行一遍 docker login ，就不用输入密码
+# 这里大家可以提前在安装依赖的时候就执行一遍 docker login ，就不用输入密码(docker login --username=waltiu registry.cn-beijing.aliyuncs.com)
 
 # 或者在执行脚本的时候输入下密码
 # echo -e "---------docker Login--------"
