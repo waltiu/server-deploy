a293c630d4fa7f59b517d309c2d6a21bd5936894
waltiu <waltiu@163.com>
2023-04-21 16:36:04 +0800

    chore: 补充log

--- watch-image/index.js
+++ watch-image/index.js
@@ -1,25 +1,47 @@
 const http = require("http");
+const url = require("url");
 
 const TYPE_POST = "POST";
 const Type_GET = "GET";
 
+const resolvePost = (req) => {
+  return new Promise((resolve) => {
+    let chunk = "";
+    req.on("data", (data) => {
+      chunk += data;
+    });
+    req.on("end", () => {
+      resolve(JSON.parse(chunk));
+    });
+  });
+};
+
 http
-  .createServer((req, res) => {
+  .createServer(async (req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
     if (req.method === Type_GET) {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      console.log(req);
-      res.end(
-        JSON.stringify({
-          data: "Hello World!",
-          url: req.url,
-          method: req.method,
-        })
-      );
-      //...
+      const { pathname, query } = url.parse(req.url, true);
+      const response = {
+        data: "Hello World!",
+        url: req.url,
+        method: req.method,
+        pathname,
+        query,
+      };
+      console.log(new Date().toLocaleString(),response)
+      res.end(JSON.stringify(response));
     }
     if (req.method === TYPE_POST) {
-      console.log("post");
-      //...
+      const data = await resolvePost(req);
+      const response= {
+        data,
+        url: req.url,
+        method: req.method,
+      }
+      console.log(new Date().toLocaleString(),response)
+      res.end(
+        JSON.stringify(response)
+      );
     }
   })
   .listen(3000, () => {
