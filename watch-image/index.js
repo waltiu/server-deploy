378c16ea060efd171b618ebe03d41949aa69fe19
waltiu <waltiu@163.com>
2023-04-21 11:11:03 +0800

    chore: 测试响应

--- watch-image/index.js
+++ watch-image/index.js
@@ -1,13 +1,20 @@
 const http = require("http");
 
-const TYPE_POST="POST"
-const Type_GET='GET'
+const TYPE_POST = "POST";
+const Type_GET = "GET";
 
 http
   .createServer((req, res) => {
-    if (req.method === Type_GET ) {
-      console.log(req.url);
-      res.end(JSON.stringify(req));
+    if (req.method === Type_GET) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      console.log(req);
+      res.end(
+        JSON.stringify({
+          data: "Hello World!",
+          url: req.url,
+          method: req.method,
+        })
+      );
       //...
     }
     if (req.method === TYPE_POST) {
